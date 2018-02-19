e55934ba43b978fab18b54ae3cd575a05f57ceb8
Danny Danger <dannywithachance@gmail.com>
2018-02-18 16:42:52 -0800

    total counter works now

--- cookie.js
+++ cookie.js
@@ -3,19 +3,24 @@ var getCookiesPerHour = function(minCustomer, maxCustomer, avgCookie) {
     return Math.floor(((Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer) * avgCookie);
 }
 
-cookieResults = [];
+
 var pioneerSquare = {
     name: "Pioneer Square",
     minCustomer: 17,
     maxCustomer: 88,
     avgCookie: 5.2,
-    cookieResults: [],
     getRandomInt: function() {
         return Math.floor(Math.random() * Math.floor(this.maxCustomer - this.minCustomer) + this.minCustomer);
     },
     getCookiesPerHour: function () {
         for (var index = 0; index < timeOfDay.length; index++) {
             return Math.floor(this.getRandomInt() * this.avgCookie);
+        }
+    },
+    calculateTotal: function() {
+        var total = 0;
+        for (var index = 0; index < cookieLocations.length; index++) {
+            total = timeOfDay.total + parseInt(cookieLocations[index].getCookiesPerHour())
         }
     }
 }
@@ -82,14 +87,19 @@ var pearlDistrict = {
     }
 }
 
-
 var cookieLocations = [pioneerSquare, portlandAirport, washingtonSquare, sellwood, pearlDistrict];
+
+
+
 var locationHTML = "";
 for (var cookieIndex = 0; cookieIndex < cookieLocations.length; cookieIndex++) {
     locationHTML += "<tr><th>"+cookieLocations[cookieIndex].name+"</th></tr>";
+    var cookieTotal = 0;
     for (var index = 0; index < timeOfDay.length; index++) {
         locationHTML += "<tr><td>"+timeOfDay[index]+"</tr></td><tr><td>"+cookieLocations[cookieIndex].getCookiesPerHour()+"</tr></td>";
+        cookieTotal += cookieLocations[cookieIndex].getCookiesPerHour();
         }
+        locationHTML += "<tr><td>Total: "+cookieTotal+"</tr></td>";
 }
 
 document.getElementById("location").innerHTML = locationHTML;
